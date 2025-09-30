08d6513702b74f9b4f315624382a9c959652c1de
ChamodiSandunika <chamodisandunika2002@gmail.com>
2025-09-30 17:11:50 +0530

    feat: Add PrizesInteractive component to the Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Hero from "../components/Hero";
 import Contact from "../components/Contact";
 import HowItWorks from "../components/HowItWorks";
+import Prizes from '@/components/PrizesInteractive';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HowItWorks />
+      <Prizes />
+      <Contact />
+
     </>
   );
 }
