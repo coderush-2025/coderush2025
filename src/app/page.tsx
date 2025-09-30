45c84755e679ebc3f489d53d9ebe2d1a70989d98
RENULUCSHMI PRAKASAN <renulucshmi@gmail.com>
2025-09-30 11:01:52 +0530

    feat: Add HowItWorks component to explain event details and registration process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "../components/Hero";
+import HowItWorks from "../components/HowItWorks";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+    </>
+  );
 }
