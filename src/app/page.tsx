75a3b2b1932abf99607a201ade66d6322746462c
Kavindya-Kariyawasam <kavindyakariyawasam01@gmail.com>
2025-10-01 20:38:41 +0530

    feat: integrate Timeline component into homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Contact from "../components/Contact";
 import Memories from "@/components/Memories";
 import HowItWorks from "../components/HowItWorks";
 import Prizes from '@/components/PrizesInteractive';
+import Timeline from "@/components/Timeline";
 
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Timeline />
       <HowItWorks />
       <Prizes />
       <Memories />
